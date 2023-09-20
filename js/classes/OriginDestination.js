276c5ad3c2f8cb25ae609cdeb7d3769d97d44561
Antonin-Winterstein <antonin.winterstein@gmail.com>
2023-09-20 20:33:08 +0200

    buttons added to change friends location, generate meeting point and generate routes + improve of design/UX

--- js/classes/OriginDestination.js
+++ js/classes/OriginDestination.js
@@ -1,23 +1,18 @@
 import { generateMatrixWithRandomValues } from "../utils/tools.js";
+import { closeModal } from "../utils/handleModal.js";
 import originDestinationData from "../../data/originDestinationData.json" assert { type: "json" };
 import routeColors from "../../data/routeColors.json" assert { type: "json" };
 
 export class OriginDestination {
 	constructor(locations, friends) {
+		// Initialize the OriginDestination class with locations and friends data
 		this.locations = locations;
 		this.friends = friends;
 
-		// Static matrix generated with random values between 5 and 60
+		// Static matrix filled manually
 		this.originDestinationMatrix = originDestinationData;
 
 		this.meetingLocation;
-
-		// // Matrix generated with random values between 5 and 60
-		// this.originDestinationMatrix = generateMatrixWithRandomValues(
-		// 	locations,
-		// 	5,
-		// 	60
-		// );
 
 		// Create a map object and set the view to a specific location and zoom level
 		this.map = L.map("map").setView([47.57, 6.86], 12);
@@ -35,32 +30,138 @@ export class OriginDestination {
 				.addTo(this.map)
 				.bindPopup(location.name); // Display the name as a popup when the marker is clicked
 		});
+
+		// Place the friends on the map
+		this.placeFriendsToLocations();
+
+		// Function that allows changing the location of friends using the form
+		this.changeFriendLocation();
+
+		// Call the function to find the meeting point when the button is clicked
+		document.querySelector("#buttonFindEarliestMeetingPoint").onclick = () => {
+			this.findEarliestMeetingPoint();
+			// Enable the button to generate routes once the meeting point is found
+			document.querySelector("#buttonGenerateRouteForFriends").disabled = false;
+		};
+
+		// Call the function to generate the routes for friends when the button is clicked
+		document.querySelector("#buttonGenerateRouteForFriends").onclick = () => {
+			this.generateRouteForFriends();
+		};
 	}
 
-	// Function to find the earliest meeting point
-	findEarliestMeetingPoint() {
-		let friendsLocation = [];
+	placeFriendsToLocations() {
+		const offset = 0.001; // Little offset to prevent overlap of friends
 
-		// Loop through all friends to store their current location in an array and show them on the map
+		// Loop through all friends to show them on the map with an offset
 		for (let x = 0; x < this.friends.length; x++) {
-			friendsLocation.push(this.friends[x].currentLocation);
 			// Custom icon of the person
 			let personIcon = L.icon({
 				iconUrl: "./media/icons/" + this.friends[x].iconUrl,
 				iconSize: [52, 52], // Adjust the size as needed
 			});
 
-			L.marker(
+			// Add a small random offset to the latitude and longitude
+			const offsetLatitude = this.friends[x].currentLocation.latitude;
+			const offsetLongitude = this.friends[x].currentLocation.longitude;
+
+			// Add the marker to the friend object in order to allow its modification
+			this.friends[x].marker = L.marker(
 				[
-					this.friends[x].currentLocation.latitude,
-					this.friends[x].currentLocation.longitude,
+					offsetLatitude + (Math.random() - 0.5) * offset,
+					offsetLongitude + (Math.random() - 0.5) * offset,
 				],
-				{ icon: personIcon }
+				{
+					icon: personIcon,
+				}
 			)
 				.addTo(this.map)
 				.bindPopup(
 					`${this.friends[x].name} is currently at ${this.friends[x].currentLocation.name}`
 				);
+		}
+	}
+
+	updateFriendMarker(friendIndex) {
+		const offset = 0.001; // Little offset to prevent overlap of friends
+		const friend = this.friends[friendIndex];
+		const location = friend.currentLocation;
+
+		// Remove the old marker from the map
+		if (friend.marker) {
+			this.map.removeLayer(friend.marker);
+		}
+
+		// Create a new marker with the updated location
+		const personIcon = L.icon({
+			iconUrl: "./media/icons/" + friend.iconUrl,
+			iconSize: [52, 52], // Adjust the size as needed
+		});
+
+		friend.marker = L.marker(
+			[
+				location.latitude + (Math.random() - 0.5) * offset,
+				location.longitude + (Math.random() - 0.5) * offset,
+			],
+			{
+				icon: personIcon,
+			}
+		)
+			.addTo(this.map)
+			.bindPopup(`${friend.name} is currently at ${location.name}`);
+
+		// Close the modal
+		closeModal();
+	}
+
+	changeFriendLocation() {
+		const form = document.querySelector("#changeFriendsLocationForm");
+		// Detect when the user submits the form
+		form.addEventListener("submit", (event) => {
+			event.preventDefault();
+			const friendIndex = parseFloat(document.querySelector("#friend").value);
+			const locationIndex = parseFloat(
+				document.querySelector("#location").value
+			);
+
+			// Disable the button to generate routes if a friend's location is changed
+			document.querySelector("#buttonGenerateRouteForFriends").disabled = true;
+
+			// Get the index of the new location
+			let index = this.locations.findIndex((location) => {
+				return (
+					location.name === this.locations[locationIndex].name &&
+					location.latitude === this.locations[locationIndex].latitude &&
+					location.longitude === this.locations[locationIndex].longitude
+				);
+			});
+
+			// Update the friend's location data
+			this.friends[friendIndex].currentLocation = this.locations[index];
+
+			// Update the friend's marker on the map
+			this.updateFriendMarker(friendIndex);
+
+			// Go back to initial view
+			this.map.setView([47.57, 6.86], 12);
+
+			// Remove the existing meeting point marker if it exists
+			if (this.meetingPointMarker) {
+				this.map.removeLayer(this.meetingPointMarker);
+			}
+
+			// Remove existing routes
+			this.removeExistingRoutes();
+		});
+	}
+
+	// Function to find the earliest meeting point
+	findEarliestMeetingPoint() {
+		let friendsLocation = [];
+
+		// Loop through all friends to store their current location in an array
+		for (let x = 0; x < this.friends.length; x++) {
+			friendsLocation.push(this.friends[x].currentLocation);
 		}
 
 		let earliestMeetingLocation = null;
@@ -87,31 +188,46 @@ export class OriginDestination {
 			}
 		}
 
+		// Remove the existing meeting point marker if it exists
+		if (this.meetingPointMarker) {
+			this.map.removeLayer(this.meetingPointMarker);
+		}
+
 		this.meetingLocation = earliestMeetingLocation;
-		// Add a popup to indicate what's the meeting point
-		L.popup()
-			.setLatLng([
-				this.meetingLocation.latitude,
-				this.meetingLocation.longitude,
-			])
-			.setContent(
+		let meetingPointIcon = L.icon({
+			iconUrl: "./media/icons/meeting.png", // Replace with your marker icon URL
+			iconSize: [52, 52],
+		});
+
+		// Create a marker for the meeting point
+		this.meetingPointMarker = L.marker(
+			[this.meetingLocation.latitude, this.meetingLocation.longitude],
+			{
+				icon: meetingPointIcon,
+			}
+		)
+			.addTo(this.map)
+			.bindPopup(
 				`The earliest meeting point is ${this.meetingLocation.name} and will take place in ${earliestMeetingTime} minutes.`
 			)
-			.openOn(this.map);
+			.openPopup(); // Open the popup when the marker is added;
 
 		// Center the map on the popup's coordinates
 		this.map.setView(
 			[this.meetingLocation.latitude, this.meetingLocation.longitude],
-			15
+			13
 		);
 
 		return { location: this.meetingLocation, time: earliestMeetingTime };
 	}
 
 	generateRouteForFriends() {
+		// Remove existing routes before generating new ones
+		this.removeExistingRoutes();
+
 		for (let x = 0; x < this.friends.length; x++) {
 			const routeColor = routeColors.colors[x % routeColors.colors.length]; // Get a color from the array
-			L.Routing.control({
+			const routingControl = L.Routing.control({
 				waypoints: [
 					// Origin is the location of the friend
 					L.latLng(
@@ -136,6 +252,19 @@ export class OriginDestination {
 					styles: [{ color: routeColor, weight: 5 }],
 				},
 			}).addTo(this.map);
+
+			// Store the routing control instance for future removal
+			this.friends[x].routingControl = routingControl;
+		}
+	}
+
+	removeExistingRoutes() {
+		// Remove existing routing controls for friends
+		for (let x = 0; x < this.friends.length; x++) {
+			const routingControl = this.friends[x].routingControl;
+			if (routingControl) {
+				this.map.removeControl(routingControl);
+			}
 		}
 	}
 }
--- js/main.js
+++ js/main.js
@@ -57,8 +57,8 @@ const matrix = new OriginDestination(locations, friendsArray);
 
 console.log(matrix);
 
-matrix.findEarliestMeetingPoint();
-matrix.generateRouteForFriends();
+// matrix.findEarliestMeetingPoint();
+// matrix.generateRouteForFriends();
 
 // console.log(
 // 	`The earliest meeting point is ${meetingLocation.location.name} and will take place in ${meetingLocation.time} minutes.`
